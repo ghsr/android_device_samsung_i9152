5144049a5adc252167b7d7dbc229d154fd0492b4
ghsr <ghsr92@yandex.ru>
2014-11-10 14:56:44 +0600

    Move screen size to cm.mk

--- cm.mk
+++ cm.mk
@@ -4,6 +4,10 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Release name
 PRODUCT_RELEASE_NAME := GT-I9152
 
+# Bootanimation
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
