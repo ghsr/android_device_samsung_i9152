2b848015b866f51a037b454960daf0e3f97451d6
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-12-16 21:35:08 +0700

    don't include GPS config
    Use the one in vendor/samsung/crater
    
    Change-Id: I159ecb0cfe5486a1ce3a708f7ef64d871b8b8849

--- device_crater.mk
+++ device_crater.mk
@@ -2,9 +2,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 LOCAL_PATH := device/samsung/crater
 
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-
 $(call inherit-product-if-exists, vendor/samsung/crater/crater-vendor.mk)
 
 # Device uses high-density artwork where available
