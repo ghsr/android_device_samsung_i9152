ce3baecd3c5d1976b71812d1388b43b799d19304
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-12-17 21:19:34 +0700

    i9152: SamsungBCMRIL: Add Samsung STK support

--- ril/com/android/internal/telephony/SamsungBCMRIL.java
+++ ril/com/android/internal/telephony/SamsungBCMRIL.java
@@ -138,6 +138,14 @@ public class SamsungBCMRIL extends RIL implements CommandsInterface {
         invokeOemRilRequestRaw(new byte[] { 'B', 'R', 'C', 'M', key, value }, response);
     }
 
+    static String
+    responseToString(int response) {
+        switch (response) {
+            case RIL_UNSOL_STK_SEND_SMS_RESULT: return "RIL_UNSOL_STK_SEND_SMS_RESULT";
+            default: return RIL.responseToString(response);
+        }
+    }
+
     protected RILRequest
     processSolicited (Parcel p) {
         int serial, error;
@@ -365,6 +373,40 @@ public class SamsungBCMRIL extends RIL implements CommandsInterface {
             }
         }
         return rr;
+    }
+
+    @Override
+    protected void
+    processUnsolicited (Parcel p) {
+        int dataPosition = p.dataPosition();
+        int response = p.readInt();
+        Object ret;
+
+        try{switch(response) {
+            case RIL_UNSOL_STK_SEND_SMS_RESULT: ret = responseInts(p); break; // Samsung STK
+            default:
+                // Rewind the Parcel
+                p.setDataPosition(dataPosition);
+
+                // Forward responses that we are not overriding to the super class
+                super.processUnsolicited(p);
+                return;
+        }} catch (Throwable tr) {
+            Rlog.e(RILJ_LOG_TAG, "Exception processing unsol response: " + response +
+                " Exception: " + tr.toString());
+            return;
+        }
+
+        switch(response) {
+            case RIL_UNSOL_STK_SEND_SMS_RESULT:
+                if (RILJ_LOGD) unsljLogRet(response, ret);
+
+                if (mCatSendSmsResultRegistrant != null) {
+                    mCatSendSmsResultRegistrant.notifyRegistrant(
+                            new AsyncResult (null, ret, null));
+                }
+            break;
+        }
     }
 
     @Override
