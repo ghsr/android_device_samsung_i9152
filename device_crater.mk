52ba06c683053e8fb57dbee38b3cf7b2e207c101
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-12-21 15:58:59 +0700

    Enable miracast
     Need to move audio_policy.conf here to fix remote submix configuration
    
    Change-Id: Icbc200ac93af614531145b33cf7af3003e50f39a

--- device_crater.mk
+++ device_crater.mk
@@ -24,7 +24,8 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
-	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+	$(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 # Prebuilt kl keymaps
 PRODUCT_COPY_FILES += \
