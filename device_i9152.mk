ddc921e64bfc21492b23eda8aeb962aff242c623
ghsr <ghsr92@yandex.ru>
2015-02-02 15:58:36 +0600

    i9152: Remove obsolete dalvik.gc.type-precise

--- device_i9152.mk
+++ device_i9152.mk
@@ -136,9 +136,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Dalvik heap config
 include frameworks/native/build/phone-hdpi-512-dalvik-heap.mk
 
-# We have enough storage space to hold precise GC data
-PRODUCT_TAGS += dalvik.gc.type-precise
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 $(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
