1ba6d339cbb0f0f25bec22f0f1eb87d1346ea888
ghsr <ghsr92@yandex.ru>
2015-03-05 16:44:05 +0600

    i9152: Cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,15 +3,23 @@ USE_CAMERA_STUB := true
 # Inherit from the proprietary version
 -include vendor/samsung/i9152/BoardConfigVendor.mk
 
-TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
+# Platform
 TARGET_BOARD_PLATFORM := capri
+
+# CPU
+TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a9
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := capri
 
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/i9152
+TARGET_KERNEL_CONFIG := cyanogenmod_i9152_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyS0,115200n8 mem=832M@0xA2000000 androidboot.console=ttyS0 vc-cma-mem=0/176M@0xCB000000
 BOARD_KERNEL_BASE := 0xa2000000
 BOARD_KERNEL_PAGESIZE := 4096
@@ -23,25 +31,40 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1142947840
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 4404019200
 BOARD_CACHEIMAGE_PARTITION_SIZE := 1073741824
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072
- 
-# Include an expanded selection of fonts
-EXTENDED_FONT_FOOTPRINT := true
-
-# Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_i9152_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/i9152
-BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/samsung/i9152/ramdisk/fstab.capri_ss_crater
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Hardware rendering
+USE_OPENGL_RENDERER := true
+BOARD_EGL_CFG := device/samsung/i9152/configs/egl.cfg
+BOARD_USE_MHEAP_SCREENSHOT := true
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS -DCAPRI_HWC -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL -DREFBASE_JB_MR1_COMPAT_SYMBOLS
+
+# CMHW
+BOARD_HARDWARE_CLASS := hardware/samsung/cmhw/ device/samsung/i9152/cmhw/
+
+# RIL
+BOARD_RIL_CLASS := ../../../device/samsung/i9152/ril/
+
+# GPS
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/i9152/include
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9152/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := device/samsung/i9152/configs/libbt_vndcfg.txt
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/i9152/bluetooth/libbt_vndcfg.txt
 
 # Connectivity - Wi-Fi
+BOARD_HAVE_SAMSUNG_WIFI             := true
 BOARD_WLAN_DEVICE                   := bcmdhd
 BOARD_WLAN_DEVICE_REV               := bcm4334
 WPA_SUPPLICANT_VERSION              := VER_0_8_X
@@ -58,53 +81,31 @@ WIFI_DRIVER_MODULE_ARG              := "firmware_path=/system/etc/wifi/bcmdhd_st
 WIFI_DRIVER_MODULE_AP_ARG           := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
 WIFI_BAND                           := 802_11_ABG
 
-# Wi-Fi Tethering
-BOARD_HAVE_SAMSUNG_WIFI := true
+# healthd
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.capri
 
-# SkTextBox for libtvout
-BOARD_USES_SKTEXTBOX := true
-
-# Hardware rendering
-USE_OPENGL_RENDERER := true
-BOARD_EGL_CFG := device/samsung/i9152/configs/egl.cfg
-BOARD_USE_MHEAP_SCREENSHOT := true
-BOARD_EGL_WORKAROUND_BUG_10194508 := true
-TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS -DCAPRI_HWC -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL -DREFBASE_JB_MR1_COMPAT_SYMBOLS
-
-# Bootanimation
-TARGET_BOOTANIMATION_PRELOAD := true
-TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+# Assert
+TARGET_OTA_ASSERT_DEVICE := crater,craterxx,i9152
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 
-# RIL
-BOARD_RIL_CLASS := ../../../device/samsung/i9152/ril/
-
-# Recovery
-TARGET_RECOVERY_FSTAB := device/samsung/i9152/ramdisk/fstab.capri_ss_crater
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-
-# Healthd
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.capri
-
-# Assert
-TARGET_OTA_ASSERT_DEVICE := crater,craterxx,i9152,I9152,GT-I9152
-
-# CMHW
-BOARD_HARDWARE_CLASS := hardware/samsung/cmhw/ device/samsung/i9152/cmhw/
-
-# GPS
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/i9152/include
- 
-# Compat
-TARGET_USES_LOGD := false
+# Bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # jemalloc causes a lot of random crash on free()
 MALLOC_IMPL := dlmalloc
+
+# Compat
+TARGET_USES_LOGD := false
+
+# SkTextBox for libtvout
+BOARD_USES_SKTEXTBOX := true
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
 
 # SELinux
 BOARD_SEPOLICY_DIRS += \
