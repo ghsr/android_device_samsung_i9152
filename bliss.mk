3e684c2468d5830c09b3fd23fd0f4b62971f2f90
ghsr <ghsr92@yandex.ru>
2015-06-19 00:18:53 +0600

    i9152: Initial Blisspop bringup

--- bliss.mk
+++ bliss.mk
@@ -1,8 +1,8 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Bootanimation
 TARGET_SCREEN_HEIGHT := 960
@@ -13,7 +13,7 @@ $(call inherit-product, device/samsung/i9152/device_i9152.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9152
-PRODUCT_NAME := cm_i9152
+PRODUCT_NAME := bliss_i9152
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := GT-I9152
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_i9152-userdebug
+add_lunch_combo bliss_i9152-userdebug
