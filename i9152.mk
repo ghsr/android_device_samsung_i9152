d0c34466f5f579f72b860a4c4f7b685fbd8c1772
ghsr <ghsr92@yandex.ru>
2015-10-01 11:44:41 +0600

    i9152: Switch to AICP

--- i9152.mk
+++ i9152.mk
@@ -1,19 +1,21 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AICP stuff
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Bootanimation
 TARGET_SCREEN_HEIGHT := 960
 TARGET_SCREEN_WIDTH := 540
+
+$(call inherit-product, vendor/aicp/configs/bootanimation.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9152/device_i9152.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9152
-PRODUCT_NAME := cm_i9152
+PRODUCT_NAME := aicp_i9152
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := GT-I9152
