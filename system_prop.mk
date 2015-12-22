4d0d620c5d05de43d20caf9b806a9f55e0e92e57
Nick Kralevich <nnk@google.com>
2015-12-22 11:02:14 -0800

    i9152: remove persist.sys.usb.config override
    The variable persist.sys.usb.config is no longer honored by the
    framework,
    and it's presence causes unnecessary toggling of the USB driver, which
    disonnects ADB and makes the device's connection unstable.
    
    Delete it.
    
    Bug: 21404762
    Bug: 18905620
    Change-Id: I78267c6c003f4c1fe0f58df7e4c4df748025080f

--- system_prop.mk
+++ system_prop.mk
@@ -2,10 +2,6 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1
 
-# Set default USB configuration
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp
-
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/system/lib/libbrcm_ril.so \
