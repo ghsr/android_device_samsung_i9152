c5f822eb0ba395e8d79828f75f433c82e3a2b84f
ghsr <ghsr92@yandex.ru>
2016-01-07 23:39:29 +0600

    i9152: Add more SELinux policies
    thanks to @pawitp for fixing gps in enforcing mode and bkmgrd denials.

--- device_i9152.mk
+++ device_i9152.mk
@@ -42,9 +42,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
-# GPS/RIL
+# Stlport
 PRODUCT_PACKAGES += \
-    libstlport \
+    libstlport
+
+# GPS
+PRODUCT_PACKAGES += \
     libglgps-compat
 
 # Open-source HALs
