05df0fee1b1adeb5d098a3a5d94630f0c7de92a4
ghsr <ghsr92@yandex.ru>
2016-04-17 22:58:20 +0600

    i9152: Add fingerprint, desc and name property

--- cm.mk
+++ cm.mk
@@ -13,3 +13,10 @@ PRODUCT_NAME := cm_i9152
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := GT-I9152
+
+# Set build fingerprint / ID / Prduct Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=craterxx \
+    TARGET_DEVICE=crater \
+    BUILD_FINGERPRINT="samsung/craterxx/crater:4.2.2/JDQ39/I9152XXUANA1:user/release-keys" \
+    PRIVATE_BUILD_DESC="craterxx-user 4.2.2 JDQ39 I9152XXUANA1 release-keys"
