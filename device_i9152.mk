055ade615d448612be0ce0a67c3e6b6ffc33acfc
ghsr <ghsr92@yandex.ru>
2016-05-04 12:31:48 +0600

    Revert "i9152: Update Wi-Fi configuration"
    This reverts commit fe73dba01fe4133c42ed5e5ebc1f2d551da5c053.

--- device_i9152.mk
+++ device_i9152.mk
@@ -39,9 +39,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
+$(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 
 # Stlport
 PRODUCT_PACKAGES += \
