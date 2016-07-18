9ce5979629ce0b411440c16836a6addca1296402
ghsr <ghsr92@yandex.ru>
2016-07-18 22:38:16 +0600

    i9152: Add OTA Server Url

--- system_prop.mk
+++ system_prop.mk
@@ -46,6 +46,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-swap=false
 
+# OTA Server Url
+PRODUCT_PROPERTY_OVERRIDES += \
+    cm.updater.uri=http://ghsr-update.appspot.com/api
+
 # Disable ADB authentication
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.adb.secure=0 \
